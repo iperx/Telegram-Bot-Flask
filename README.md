2a61e837ddbf486cd520556d51d950fc40eeda00
Igor Permyakov <imouttasightx@gmail.com>
2020-05-31 01:04:30 +0600

    v1.3

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # Telegram-Bot-Flask
 
-### Tells what's the weather in any city at the moment.
+* Tells what's the weather in any city at the moment
+* Gives the today's exchange rate
+* Contains the simple chat for users
 
-### Usage:
-* Press /weather to start a short dialogue with the bot
-* Specify the city to search
-* Profit!
+### Weather
+**Source**: <https://openweathermap.org/api>
+**Commands**:
+* /weather
+
+### Exchange rate
+**Source**: <https://www.cbr-xml-daily.ru/>
+**Commands**:
+* /exrate
+
+### Chat
+**Commands**:
+* /join
+* /exit
+* /whoisup
+
+### Customization:
+1. Create your bot with @BotFather (you'll receive a token)
+2. Register an account on the weather website and receive an API token
+3. Create file _misc.py_ in the _app_ folder beside the _main.py_
+4. Put `TG_BOT_TOKEN = 'your-bot-token'` into the _misc.py_
+5. Put `WEATHER_API_KEY = 'your-weather-token'` down below
+6. Get hosting (e.g. <https://www.pythonanywhere.com/>)
--- app/main.py
+++ app/main.py
@@ -14,14 +14,20 @@ app = Flask(__name__)
 sslify = SSLify(app)
 URL = 'https://api.telegram.org/bot{}/'.format(TG_BOT_TOKEN)
 tasks = {} # "chat_id": task
+chat_members = set() # new
+bot_commands = {'/weather', '/exrate', '/join', '/exit', '/whoisup'}
 
 
 def write_json(data, filename='answers.json'):
+    """Dumps json data to a file.
+    """
     with open(filename, 'a') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 def send_message(chat_id, text='blablabla'):
+    """Sends the text message to the particular private chat.
+    """
     url = URL + 'sendMessage'
     answer = {
         'chat_id': chat_id,
@@ -32,7 +38,7 @@ def send_message(chat_id, text='blablabla'):
 
 
 def parse_user_text(text):
-    """Takes user message and looks for a city_name.
+    """Takes user's text message and looks for a city_name.
     Returns the city name or None.
     """
     pattern = r'^[a-zA-Zа-яА-ЯёЁ]{1,30}[\s-]?[a-zA-Zа-яА-ЯёЁ]{,30}[\s-]?[a-zA-Zа-яА-ЯёЁ]{,30}'
@@ -43,6 +49,10 @@ def parse_user_text(text):
 
 
 def get_weather(city):
+    """Takes the city name.
+    Returns current weather in degrees Celsius or the message,
+    saying that the required city hasn't been found.
+    """
     api_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         'q': city,
@@ -61,6 +71,9 @@ def get_weather(city):
 
 
 def get_exchange_rate():
+    """Returns current exchange rate in rubles for 8 currencies,
+    sorted alphabetically.
+    """
     url = 'https://www.cbr-xml-daily.ru/daily_json.js'
     summary = {}
     valutes = {
@@ -78,37 +91,74 @@ def get_exchange_rate():
         if valute in valutes:
             summary[valutes[valute]] = info['Value'] / info['Nominal']
             
-    answer = 'Date: {}\n\n'.format(res['Date'].replace('T', ' '))
+    answer = '>This is what we have today:\n\n'
     for name in sorted(summary.keys()):
         answer += '{} = {:.2f} rub\n\n'.format(name, summary[name])
 
     return answer.rstrip()
 
 
+def send_msg_to_all(user, message, m_type='normal'):
+    """Sends the message to chat users.
+    The argument 'm_type':
+    'system' - for system messages
+    'normal' - for regular messages from one user to all others except himself
+    """
+    if m_type == 'system':
+        for person in chat_members:
+            text = '*{} {}*'.format(user['first_name'], message)
+            send_message(person, text=text)
+    else:
+        for person in chat_members:
+            if person != user['id']:
+                text = user['first_name'] + ':\n' + message
+                send_message(person, text=text)
+
+
 @app.route('/{}/'.format(TG_BOT_TOKEN), methods=['POST', 'GET'])
 def index():
+    """Reacts to all the possible commands.
+    I know this function does almost everything TODO: fix it ASAP
+    """
     if request.method == 'POST':
         r = request.get_json()
-        chat_id = r['message']['chat']['id']
-        message = r['message']['text']
+        if 'text' in r['message']:
+            chat_id = r['message']['chat']['id']
+            message = r['message']['text']
+            user = r['message']['from']
 
-        if chat_id not in tasks:
-            if re.search(r'/weather', message) or re.search(r'/погода', message):
-                task = Task(chat_id, message)
-                send_message(task.chat_id, text='The weather in which city is interesting to you?')
-                tasks[task.chat_id] = task
-            elif re.search(r'/exrate', message):
-                exrate = get_exchange_rate()
-                send_message(chat_id, text=exrate)
-        else:
-            task = tasks[chat_id]
-            city = parse_user_text(message)
-            if city is not None:
-                weather = get_weather(city)
-                send_message(task.chat_id, text=weather)
+            if user['id'] in chat_members and message not in bot_commands:
+                send_msg_to_all(user, message)
+
+            if chat_id not in tasks:
+                if message == '/weather':
+                    task = Task(chat_id, 'weather')
+                    send_message(task.chat_id, text='The weather in which city is interesting to you?')
+                    tasks[task.chat_id] = task
+                elif message == '/exrate':
+                    exrate = get_exchange_rate()
+                    send_message(chat_id, text=exrate)
+                elif message == '/join':
+                    if user['id'] not in chat_members:
+                        chat_members.add(user['id'])
+                        send_msg_to_all(user, 'enters the chat', 'system')
+                elif message == '/exit':
+                    chat_members.discard(user['id'])
+                    send_msg_to_all(user, 'leaves the chat', 'system')
+                elif message == '/whoisup':
+                    ids_up = '\n'.join([str(m) for m in chat_members])
+                    send_message(chat_id, text=ids_up)
             else:
-                send_message(task.chat_id, text='Incorrect city name.')
-            tasks.pop(chat_id, None)
+                task = tasks[chat_id]
+                if task.message == 'weather':
+                    city = parse_user_text(message)
+                    if city is not None:
+                        weather = get_weather(city)
+                        send_message(task.chat_id, text=weather)
+                    else:
+                        send_message(task.chat_id, text='Incorrect city name.')
+                    tasks.pop(chat_id, None)
+
         return jsonify(r)
 
     return '<h1>Bot welcomes you</h1>'
